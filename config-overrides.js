cb73b355bf7f4d8ac1b5563364b9cc7d3e6689b4
lqh <15017662461@163.com>
2020-10-14 20:42:08 +0800

    添加装饰器写法

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
 //基于react-app-rewired和 customize-cra的配置文件，与craco.config二选一即可
 //选择本文件需要将package.json中的start等命令改为react-app-rewired start...
-// 安装的插件有：antd、babel-plugin-import、less less-loader、customize-cra、react-app-rewired
+// 安装的插件有：antd、babel-plugin-import、less less-loader、customize-cra、react-app-rewired、@babel/plugin-proposal-decorators
 const {override, addDecoratorsLegacy,addLessLoader,fixBabelImports} = require('customize-cra');
 const modifyVars = require('./lessVars');
 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import {Button} from 'antd';
+import React, { Component } from 'react';
+import {
+  Button
+} from 'antd';
 
-function App() {
-  return (
-    <div className="App">
+const testHOC = (WrappedComponent) => {
+  return class HOCComponent extends Component{
+    render(){
+      return(
+        <>
+          <WrappedComponent></WrappedComponent>
+          <div>
+            这是高阶组件的信息
+          </div>
+        </>
+      )
+    }
+  }
+}
+
+@testHOC
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <Button type="primary">按钮</Button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
+
+
